85c7e3199781e59de0a24f7eff6c78a15efde52d
ThilinaJayamal <thilinajayamal271@gmail.com>
2025-12-30 23:55:34 +0530

    loading issue fix

--- src/components/PageLoader.jsx
+++ src/components/PageLoader.jsx
@@ -3,7 +3,7 @@ import LoadingSpinner from './LoadingSpinner'
 
 function PageLoader({ text = 'Loading...' }) {
     return (
-        <div className='w-full min-h-screen flex gap-3 flex-col items-center justify-center bg-gradient-to-br from-blue-50 to-purple-50'>
+        <div className='w-full min-h-screen flex gap-3 flex-col items-center justify-center bg-linear-to-br from-blue-50 to-purple-50'>
             <div className='text-center space-y-4'>
                 <h1 className='text-5xl font-bold text-gray-900'>Quiz<span className='text-blue-500'>Master</span></h1>
                 <LoadingSpinner size="large" text={text} />
